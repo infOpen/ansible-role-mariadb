2c53c0185f8fb9fe111439268a87fae5b81ecb70
Alexandre Chaussier <a.chaussier@infopen.pro>
2016-10-31 00:38:06 +0100

    Add packages installation management

--- tests/testinfra/test_installation.py
+++ tests/testinfra/test_installation.py
@@ -9,8 +9,35 @@ pytestmark = pytest.mark.docker_images(
     'infopen/ubuntu-xenial-ssh-py27:0.2.0'
 )
 
-def test_foo_a(User):
-    assert User().name == 'root'
 
-def test_foo_b(User):
-    assert User().name == 'root'
+def test_packages(Package):
+
+    packages = [
+        'mariadb-client',
+        'mariadb-server',
+        'python-mysqldb',
+        'software-properties-common',
+    ]
+
+    for package in packages:
+        assert Package(package).is_installed
+
+        if 'mariadb-' in package:
+            assert '10.1.' in Package(package).version
+
+
+def test_apt_repository_file(SystemInfo, File):
+
+    apt_file = File('/etc/apt/sources.list.d/mariadb.list')
+
+    expected_content = (
+        "deb [arch=amd64] "
+        "http://fr.mirror.babylon.network/mariadb/repo/10.1/{} "
+        "{} main".format(SystemInfo.distribution, SystemInfo.codename))
+
+    assert apt_file.exists
+    assert apt_file.is_file
+    assert apt_file.user == 'root'
+    assert apt_file.group == 'root'
+    assert apt_file.mode == 0o644
+    assert expected_content in apt_file.content_string
